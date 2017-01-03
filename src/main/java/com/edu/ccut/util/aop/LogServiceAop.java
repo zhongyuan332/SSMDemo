f4f28f5673ccc527cce8c394472f4c8d2e28b398
zhongyuan332 <zhongyuan332@163.com>
2017-01-03 11:23:17 +0800

    定义切面aop，添加日志记录

--- src/main/java/com/edu/ccut/util/aop/LogServiceAop.java
+++ src/main/java/com/edu/ccut/util/aop/LogServiceAop.java
@@ -1,9 +1,37 @@
 package com.edu.ccut.util.aop;
 
 import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
+@Component // 注入依赖
+@Aspect // 该注解标示该类为切面类
 public class LogServiceAop {
 	private static final Logger logger = Logger.getLogger(LogServiceAop.class);
-	
-	
+
+	@Pointcut("execution(* com.edu.ccut.service.impl.*ServiceImpl.*(..))")
+	public void logAop(JoinPoint jp) {
+	}
+
+	@Before("logAop() && args(name)")
+	public void logBefore(String name) {
+		System.out.println(name + "前置通知Before");
+	}
+
+	@AfterReturning("logAop()")
+	public void logAfterReturning() {
+		System.out.println("返回通知AfterReturning");
+	}
+
+	@After("logAop() && args(name)")
+	public void logAfter(String name) {
+		System.out.println(name + "后置通知After");
+	}
+
+	@AfterThrowing("logAop()")
+	public void logAfterThrow() {
+		System.out.println("异常通知AfterThrowing");
+	}
+
 }
